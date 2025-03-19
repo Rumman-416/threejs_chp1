cb703f734237e11889f06ef973a7295048b1e58a
Rumman <rumman.realatte@gmail.com>
2025-03-19 18:26:49 +0530

    video 6

--- src/main.js
+++ src/main.js
@@ -8,25 +8,26 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: "red" });
 const mesh = new THREE.Mesh(geometry, material);
-mesh.position.set(1, 1, 1);
-mesh.scale.x = 2;
+mesh.position.set(0, 0, 0);
+// mesh.scale.x = 2;
 
-mesh.rotation.reorder("yxz");
-mesh.rotation.x = 3;
-mesh.rotation.y = Math.PI / 2;
+// mesh.rotation.reorder("yxz");
+// mesh.rotation.x = 3;
+// mesh.rotation.y = Math.PI / 2;
 
 scene.add(mesh);
-mesh.position.normalize();
-console.log(mesh.position.length());
+// mesh.position.normalize();
 
-const group = new THREE.Group();
-scene.add(group);
-const cube1 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: "cyan" })
-);
-cube1.position.x = -1;
-group.add(cube1);
+//group
+// const group = new THREE.Group();
+// scene.add(group);
+// const cube1 = new THREE.Mesh(
+//   new THREE.BoxGeometry(1, 1, 1),
+//   new THREE.MeshBasicMaterial({ color: "cyan" })
+// );
+// cube1.position.x = -1;
+// group.add(cube1);
+
 //sizes
 const sizes = {
   width: 800,
@@ -34,15 +35,15 @@ const sizes = {
 };
 
 //axis helper
-const axesHelper = new THREE.AxesHelper();
-scene.add(axesHelper);
+// const axesHelper = new THREE.AxesHelper();
+// scene.add(axesHelper);
 
 //camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 // camera.position.z = 3;
 camera.position.set(0, 0, 3);
 scene.add(camera);
-camera.lookAt(mesh.position);
+// camera.lookAt(mesh.position);
 
 //renderer
 const canvas = document.querySelector(".webgl");
@@ -51,3 +52,32 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
+
+//Time
+let time = Date.now();
+
+//Clock
+const clock = new THREE.Clock();
+
+//Animation
+const tick = () => {
+  //clock
+  const elapsedTime = clock.getElapsedTime();
+
+  //time
+  //   const currentTime = Date.now();
+  //   const deltaTime = currentTime - time;
+  //   time = currentTime;
+
+  //   console.log(deltaTime);
+
+  //update object
+  //   mesh.rotation.y += 0.001 * deltaTime;
+  mesh.rotation.y = elapsedTime;
+
+  //renderer
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+
+tick();
